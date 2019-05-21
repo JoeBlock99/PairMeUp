61c55584a249d943affc8a0b98c93f8157747efc
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2019-05-21 17:31:42 -0600

    Comienzo

---
+++ Fase 2/Proyecto2parte2.py
@@ -0,0 +1,115 @@
+##  Universidad del Valle de Guatemala
+##  Proyecto 2 Estructuras - Pair Me Up
+##  Andrés Berthet - 171504
+##  José Block - 18935
+##  Luis Pedro Cuéllar - 18220
+
+## Print menu
+##  1. Iniciar Sesión
+##  2. Registrarse
+
+##  if 1: Check if user exists
+##  if 2: Print the proces to new account
+
+##  NEW USER:
+##  Prergunntar nombre
+##  Preguntar correo de la Universidad
+##  Empezara a hacer las preguntas e ir guardando las respuestas
+print("--------------------------------Bienvenido-a-PairMeUp--------------------------------\n\n")
+##  SAME USER:
+##  Print Users
+print("     Escoja alguna de las opciones, porfavor.\n")
+print("     Opciones:\n     1. Ver mis resultados\n     2. Registrarse\n")
+opcion1 = input()
+if opcion1 == "1":
+    print("     Ingrese su nombre:\n")
+    #calcular porcentajes
+elif opcion1  == "2":
+    print("Ingrese el número correspondiente a la respuesta de cada pregunta.\n")
+    ptint("Tipo de comida favorita:\n1. italiano\n2. chino\n3. mariscos\n4. típico\n5. mexicano\n6. americano\n")
+    print("\nPreferencia de Música:\n1. Pop\n2. Rock\n3. Electrónica\n4. Country\n5. Reggaeton\n6. Rap"\n)
+    Print("")
+
+
+Preferencias de Redes
+Instagram
+Snapchat
+Facebook
+Twitter
+WhatsApp
+YouTube
+Preferencias en Películas
+Acción
+Comedia
+Romance
+Terror
+Aventura
+Guerra
+Sci-Fi
+Fantasía
+Documental
+Preferencias de Series
+Acción
+Comedia
+Romance
+Terror
+Aventura
+Guerra
+Sci-Fi
+Fantasía
+Documental
+Segundo Idioma
+Inglés
+Alemán
+Italiano
+Francés
+Portugués
+Japonés
+Mandarin
+Sólo Español
+Preferencia de Bebidas
+Frutales
+Limonada/Naranjada
+Gaseosa
+Cerveza
+Whiskey
+Vino
+Cafe
+Te
+Preferencia de Deportes
+Fútbol
+Fútbol Americano
+Voleibol
+Baloncesto
+Hockey
+Golf
+Ni uno
+Preferencia de Entretenimiento
+VideoJuegos
+Series
+Películas
+Teatro
+Deportes
+Dibujo/Arte
+Leer
+Salidas Sociales
+Preferencia de Animales
+Perros
+Gatos
+Conejos
+Hamster
+Pez
+Pájaros
+Horse
+Reptil
+Preferencia Sexuales
+Mujer - Hombre
+Mujer - Mujer
+Hombre - Hombre
+Ambas
+Razones de usar el app
+Conocer personas
+Crear amistades
+Relaciones Serias
+Relaciones No Serias
+")
